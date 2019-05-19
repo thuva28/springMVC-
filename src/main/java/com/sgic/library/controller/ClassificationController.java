90186fb4191ee03423da26bf4d7333c7a75a4e0a
thuva03 <raganthiva@gmail.com>
2019-05-19 15:30:12 +0530

    mvc

--- src/main/java/com/sgic/library/controller/ClassificationController.java
+++ src/main/java/com/sgic/library/controller/ClassificationController.java
@@ -1,7 +1,26 @@
 package com.sgic.library.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.sgic.library.model.Classification;
+import com.sgic.library.service.ClassificationService;
+
+@Controller
 public class ClassificationController {
 	
+	@Autowired
+	private ClassificationService classificationService;
+
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String printWelcome(Model model) {
+		model.addAttribute("classification", new Classification());
+		return "addclassification";
+	}
 	
 
 }
