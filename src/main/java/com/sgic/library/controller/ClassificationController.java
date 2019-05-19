7e7466746928c0017affe521c387cd5fba0db99f
thuva03 <raganthiva@gmail.com>
2019-05-19 17:04:40 +0530

    mvc

--- src/main/java/com/sgic/library/controller/ClassificationController.java
+++ src/main/java/com/sgic/library/controller/ClassificationController.java
@@ -3,6 +3,7 @@ package com.sgic.library.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -22,5 +23,11 @@ public class ClassificationController {
 		return "addclassification";
 	}
 	
+	@RequestMapping(value ="/save", method =RequestMethod.POST)
+    public String save(@ModelAttribute("classification") Classification classification) {
+		System.out.println(classification.getClassificationName());
+		classificationService.addClassification(classification);
+		return null;
+	}
 
 }
